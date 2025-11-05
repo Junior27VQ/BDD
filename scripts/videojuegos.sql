baa750c398eb0da8b6d25e65017e25b100af3bcc
Junior27VQ <favq1999@outlook.com>
2025-11-05 13:58:53 -0500

    Añadido el archivo de la base de datos: videojuegos.sql

---
+++ scripts/videojuegos.sql
@@ -0,0 +1,8 @@
+--crear tabla de videojuegos
+create table videojuegos(
+    codigo int not null,
+	nombre varchar(100) not null,
+	descripcion varchar(300) not null,
+	valoracion int not null,
+	constraint videojuego_pk primary key(codigo)
+)
