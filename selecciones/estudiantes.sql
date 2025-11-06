9e4b3ee5b85bc2175b5be1c7f872a76feda96bae
Junior27VQ <favq1999@outlook.com>
2025-11-06 11:31:16 -0500

    seleccion de estudiante

---
+++ selecciones/estudiantes.sql
@@ -0,0 +1,8 @@
+--selecion de estudiantes
+select nombre,cedula from estudiantes
+
+select nombre from estudiantes
+where cedula like('17%')
+
+select nombre,apellido from estudiantes
+where nombre like('A%')
