21818ed44c6e8f0afbc71b0b21f5dc66b03342c0
Junior27VQ <favq1999@outlook.com>
2025-11-06 11:54:58 -0500

    seleccion de vidojuegos

---
+++ selecciones/videojuegos.sql
@@ -0,0 +1,9 @@
+--seleccionar videojuegos
+select * from videojuegos
+where nombre like('C%')
+
+select * from videojuegos
+where valoracion between'9'and'10'
+
+select * from videojuegos
+where descripcion is null
