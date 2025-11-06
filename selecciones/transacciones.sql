dff695abea2d41c0f730337d4c8bcbfc2ee46f7e
Junior27VQ <favq1999@outlook.com>
2025-11-06 12:03:25 -0500

    seleccion de transacciones

---
+++ selecciones/transacciones.sql
@@ -0,0 +1,9 @@
+--seleccion  de transacciones
+select * from transacciones
+where tipo like('D')
+
+select * from transacciones
+where monto between'200' and'2000'
+
+select codigo,monto,tipo,fecha from transacciones
+where fecha is not null
