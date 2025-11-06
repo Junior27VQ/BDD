64e2ba93806e5b311b75c7a9e7278e9780bfef12
Junior27VQ <favq1999@outlook.com>
2025-11-06 11:43:20 -0500

    seleccion de registros_entradas

---
+++ selecciones/registros_entradas.sql
@@ -0,0 +1,8 @@
+--seleccion de registro_entrada
+select cedula_empleado,fecha,hora from registro_entrada
+
+select cedula_empleado,fecha,hora from registro_entrada
+where hora between'07:00' and'14:00'
+
+select cedula_empleado,fecha,hora from registro_entrada
+where hora>'08:00'
